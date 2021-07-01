edacbe55e570e5b3d7e517949f4dce58962ede98
Hacker_zzm <zzmhacker@gmail.com>
2021-07-01 17:16:20 +0800

    下班备份

--- app/src/main/java/com/home/zzm/apg202107/MainActivity.kt
+++ app/src/main/java/com/home/zzm/apg202107/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.home.zzm.apg202107
 
+import android.media.MediaPlayer
+import android.media.MediaPlayer.OnPreparedListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RelativeLayout
+import android.widget.VideoView
 
 class MainActivity : AppCompatActivity() {
+  private var videoview:VideoView? = null
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    videoview = findViewById(R.id.videoview)
+    //对于全屏拉伸操作，主要就是如下代码
+    var layoutParams = RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.FILL_PARENT, RelativeLayout.LayoutParams.FILL_PARENT)
+    layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+    layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP)
+    layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
+    layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
+    videoview?.layoutParams = layoutParams
+    videoview?.setVideoPath("/sdcard/ZXAdvertisement/奥克斯广场.avi")
+    videoview?.setOnPreparedListener { videoview?.start() }
   }
 }
